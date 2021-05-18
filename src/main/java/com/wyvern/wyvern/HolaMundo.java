a96ebd9bb064793f7395a3d22473e0e03f760a6b
ChriBiels <christiangfbielsa@gmail.com>
2021-05-18 23:30:31 +0200

    Se añade contador Consonante/Vocal

--- src/main/java/com/wyvern/wyvern/HolaMundo.java
+++ src/main/java/com/wyvern/wyvern/HolaMundo.java
@@ -1,6 +1,8 @@
 package com.wyvern.wyvern;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
@@ -15,4 +17,40 @@ public class HolaMundo {
    public String despidete(){
        return "Adios amigo";
    }
+
+   @GetMapping("/saludame/{nombre}")
+   public String saludame(@PathVariable String nombre){
+       return "Hola que tal " + nombre;
+   }
+
+   @GetMapping("/contar/{palabra}")
+   public String contar(@PathVariable String palabra){
+    char[] vocales ={'a','e','i','o','u'};
+    char[] consonantes={'b','c','d','f','g','h','j','k','l','m','n',
+                        'ñ','q','r','s','t','v','w','x','y','z'};
+    int numeroVocales=0;
+    int numeroConsonantes=0;
+    for(int i=0;i<palabra.length();i++){
+            for(int j=0 ; j<vocales.length ;j++){
+                if(palabra.charAt(i) == vocales[j]){
+                    numeroVocales++;
+
+                }
+
+            }
+
+            for(int j=0 ; j<consonantes.length;j++){
+                if(palabra.charAt(i) == consonantes[j]){
+                    numeroConsonantes++;
+
+                }
+
+            }
+    }
+    return ("Vocales("+numeroVocales+")"+"\n"+"Consonantes("+numeroConsonantes+")");
+
+   }
+
 }
+
+
