04ad8e079ceada489c76ebc2541c3b71a6877f4d
ChriBiels <christiangfbielsa@gmail.com>
2021-05-18 18:57:31 +0200

    Añadiendo Springboot

---
+++ src/main/java/com/wyvern/wyvern/WyvernApplication.java
@@ -0,0 +1,13 @@
+package com.wyvern.wyvern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyvernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyvernApplication.class, args);
+	}
+
+}
